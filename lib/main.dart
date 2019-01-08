180d83e3fe17bed41e44cffe3371665c1bece423
terrypls <selvastian@gmail.com>
2019-01-08 12:20:54 -0300

    ejemplo wifi

---
+++ lib/main.dart
@@ -0,0 +1,158 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wifi/wifi.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Wifi',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _wifiName = 'click button to get wifi ssid.';
+  String _ip = 'click button to get ip.';
+  List ssidList = [];
+  String ssid = '', password = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wifi'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: ListView.builder(
+          padding: EdgeInsets.all(8.0),
+          itemCount: ssidList.length + 1,
+          itemBuilder: (BuildContext context, int index) {
+            return itemSSID(index);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget itemSSID(index) {
+    if (index == 0) {
+      return Column(
+        children: [
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('ssid'),
+                onPressed: _getWifiName,
+              ),
+              Text(_wifiName),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text('ip'),
+                onPressed: _getIP,
+              ),
+              Text(_ip),
+            ],
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: UnderlineInputBorder(),
+              filled: true,
+              icon: Icon(Icons.wifi),
+              hintText: 'Your wifi ssid',
+              labelText: 'ssid',
+            ),
+            keyboardType: TextInputType.text,
+            onChanged: (value) {
+              ssid = value;
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: UnderlineInputBorder(),
+              filled: true,
+              icon: Icon(Icons.lock_outline),
+              hintText: 'Your wifi password',
+              labelText: 'password',
+            ),
+            keyboardType: TextInputType.text,
+            onChanged: (value) {
+              password = value;
+            },
+          ),
+          RaisedButton(
+            child: Text('connection'),
+            onPressed: connection,
+          ),
+        ],
+      );
+    } else {
+      return Column(children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.wifi),
+          title: Text(
+            ssidList[index - 1],
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: 16.0,
+            ),
+          ),
+          dense: true,
+        ),
+        Divider(),
+      ]);
+    }
+  }
+
+  void loadData() {
+    Wifi.list('').then((list) {
+      setState(() {
+        ssidList = list;
+      });
+    });
+  }
+///funciona la libreria de wifi
+  Future<Null> _getWifiName() async {
+    String wifiName = await Wifi.ssid;
+    setState(() {
+      _wifiName = wifiName;
+      print(wifiName);
+    });
+  }
+
+  Future<Null> _getIP() async {
+    String ip = await Wifi.ip;
+    setState(() {
+      _ip = ip;
+    });
+  }
+
+  Future<Null> connection() async {
+    Wifi.connection(ssid, password).then((v) {
+      print(v);
+    });
+  }
+}
